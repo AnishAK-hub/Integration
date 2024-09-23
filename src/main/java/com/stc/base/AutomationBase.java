7d5ad6fa2ea9dfeafd588f10d169140ffc692412
AnishAK-hub <anishstc67@gmail.com>
2024-09-23 15:50:46 +0530

    4\5

--- src/main/java/com/stc/base/AutomationBase.java
+++ src/main/java/com/stc/base/AutomationBase.java
@@ -15,42 +15,52 @@ public class AutomationBase {
     public WebDriver driver;
 
     /**
-     * Method to start Browser session with Edge as the first priority, then Firefox
+     * Method to start Browser session for Edge/Firefox in headless mode (suitable for Jenkins).
      * 
      * @author Anish
      * @since 03/October/2023
      * @param browserName
-     * @return
+     * @return WebDriver instance
      * @throws IOException
      */
     public WebDriver startBrowserSession(String browserName) throws IOException {
+
         System.out.println("Launching browser: " + browserName);
 
-        if (browserName.equalsIgnoreCase("edge")) {
+        // Edge Browser Setup
+        if (browserName.equalsIgnoreCase("edge") || browserName.equalsIgnoreCase("Edge_headless")) {
             EdgeOptions options = new EdgeOptions();
             options.addArguments("--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage", "--guest");
+            if (browserName.equalsIgnoreCase("Edge_headless")) {
+                options.addArguments("--headless");  // Enable headless mode for Jenkins
+            }
             WebDriverManager.edgedriver().setup();
             driver = new EdgeDriver(options);
 
-        } else if (browserName.equalsIgnoreCase("firefox")) {
+        // Firefox Browser Setup
+        } else if (browserName.equalsIgnoreCase("firefox") || browserName.equalsIgnoreCase("Firefox_headless")) {
             FirefoxOptions options = new FirefoxOptions();
+            if (browserName.equalsIgnoreCase("Firefox_headless")) {
+                options.addArguments("--headless");  // Enable headless mode for Jenkins
+            }
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver(options);
 
         } else {
             System.out.println("Unsupported browser: " + browserName);
-            return null; // Return null if browser is unsupported
         }
 
+        // Maximize browser window and set implicit wait
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         return driver;
     }
 
+    // Tear down method to close browser session after tests
     @AfterClass
     public void tearDown() throws InterruptedException {
         if (driver != null) {
-            Thread.sleep(2000);
+            Thread.sleep(2000);  // Delay for observation
             driver.quit();
         }
     }
