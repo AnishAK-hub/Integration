62de65ac7a32f4ffb2485af17c907eec3a99e695
AnishAK-hub <anishstc67@gmail.com>
2024-09-23 15:33:53 +0530

    4

--- src/main/java/com/stc/base/AutomationBase.java
+++ src/main/java/com/stc/base/AutomationBase.java
@@ -24,30 +24,22 @@ public class AutomationBase {
      * @throws IOException
      */
     public WebDriver startBrowserSession(String browserName) throws IOException {
-
         System.out.println("Launching browser: " + browserName);
 
-        if (browserName.equalsIgnoreCase("edge") || browserName.equalsIgnoreCase("Edge_headless")) {
-            // First Priority: Edge Browser
+        if (browserName.equalsIgnoreCase("edge")) {
             EdgeOptions options = new EdgeOptions();
             options.addArguments("--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage", "--guest");
-            if (browserName.equalsIgnoreCase("Edge_headless")) {
-                options.addArguments("--headless");
-            }
             WebDriverManager.edgedriver().setup();
             driver = new EdgeDriver(options);
 
-        } else if (browserName.equalsIgnoreCase("firefox") || browserName.equalsIgnoreCase("Firefox_headless")) {
-            // Second Priority: Firefox Browser
+        } else if (browserName.equalsIgnoreCase("firefox")) {
             FirefoxOptions options = new FirefoxOptions();
-            if (browserName.equalsIgnoreCase("Firefox_headless")) {
-                options.addArguments("--headless");
-            }
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver(options);
 
         } else {
             System.out.println("Unsupported browser: " + browserName);
+            return null; // Return null if browser is unsupported
         }
 
         driver.manage().window().maximize();
